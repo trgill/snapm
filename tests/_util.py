17841da095c6b10a8eedc1021a0e0de46c8b394c
Bryn M. Reeves <bmr@redhat.com>
2024-02-16 17:56:31 +0000

    Add infrastructure for roll back tests
    Add helper methods to LvmLoopBacked to enable initiating and
    testing for the effects of a rollback operation.

--- tests/_util.py
+++ tests/_util.py
@@ -1,5 +1,6 @@
 import tempfile
 import subprocess
+from pathlib import Path
 import os
 
 _LOSETUP_CMD = "losetup"
@@ -7,6 +8,7 @@ _LOSETUP_CMD = "losetup"
 _VGCREATE_CMD = "vgcreate"
 _LVCREATE_CMD = "lvcreate"
 _VGREMOVE_CMD = "vgremove"
+_VGCHANGE_CMD = "vgchange"
 _LVS_CMD = "lvs"
 _LVM_CMD = "lvm"
 _LVM_VERSION = "version"
@@ -191,20 +193,41 @@ class LvmLoopBacked(object):
             [_MOUNT_CMD, f"/dev/{_VG_NAME}/{name}", f"{self.mount_root}/{name}"]
         )
 
+    def mount_all(self):
+        for lv in self.all_volumes():
+            self.mount(lv)
+
     def umount(self, name):
         subprocess.check_call([_UMOUNT_CMD, f"{self.mount_root}/{name}"])
 
+    def umount_all(self):
+        for lv in self.all_volumes():
+            self.umount(lv)
+
+    def activate(self):
+        subprocess.check_call([_VGCHANGE_CMD, "-ay", _VG_NAME])
+
+    def deactivate(self):
+        subprocess.check_call([_VGCHANGE_CMD, "-an", _VG_NAME])
+
     def mount_points(self):
         return [f"{self.mount_root}/{name}" for name in self.all_volumes()]
+
+    def touch_path(self, relpath):
+        path = Path(f"{self.mount_root}/{relpath}")
+        path.touch()
+
+    def test_path(self, relpath):
+        path = Path(f"{self.mount_root}/{relpath}")
+        return path.exists()
 
     def dump_lvs(self):
         subprocess.check_call([_LVS_CMD])
 
     def destroy(self):
+        self.umount_all()
         for lv in self.all_volumes():
-            self.umount(lv)
             os.rmdir(f"{self.mount_root}/{lv}")
-
         os.rmdir(self.mount_root)
 
         subprocess.check_call([_VGREMOVE_CMD, "--force", "--yes", _VG_NAME])
