383ea42cfd3d602ff2f98407b2ba8c2ff4d2dc60
Bryn M. Reeves <bmr@redhat.com>
2024-06-21 18:38:09 +0100

    snapm: disable flake8 F401, F403 in snapm/__init__.py imports

--- snapm/__init__.py
+++ snapm/__init__.py
@@ -14,7 +14,7 @@
 """
 Snapm top-level package.
 """
-from ._snapm import *
-from ._snapm import __all__
+from ._snapm import *  # noqa: F401, F403
+from ._snapm import __all__  # noqa: F401
 
 __version__ = "0.3.0"
