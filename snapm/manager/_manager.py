65ad29a610e4b8385126eff150223f93cd68446b
Bryn M. Reeves <bmr@redhat.com>
2024-12-03 19:22:10 +0000

    snapm: Manager.delete_snapshot_sets(): fix formatting

--- snapm/manager/_manager.py
+++ snapm/manager/_manager.py
@@ -1026,10 +1026,10 @@ class Manager:
                     f"Snapshots from snapshot set {snapset.name} are mounted: cannot delete"
                 )
             if snapset.status == SnapStatus.REVERTING:
-                _log_error("Cannot operate on reverting snapshot set '%s'", snapset.name)
-                raise SnapmBusyError(
-                    f"Failed to delete snapshot set {snapset.name}"
+                _log_error(
+                    "Cannot operate on reverting snapshot set '%s'", snapset.name
                 )
+                raise SnapmBusyError(f"Failed to delete snapshot set {snapset.name}")
             delete_snapset_boot_entry(snapset)
             delete_snapset_revert_entry(snapset)
             for snapshot in snapset.snapshots.copy():
