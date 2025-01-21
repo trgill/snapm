18aebae1b34658fbfe6a8e20b52424c531071cf0
Bryn M. Reeves <bmr@redhat.com>
2025-01-21 16:13:56 +0000

    tests: add explicit log formatter to file and console handlers

--- tests/__init__.py
+++ tests/__init__.py
@@ -18,8 +18,13 @@ import time
 
 log = logging.getLogger()
 log.setLevel(logging.DEBUG)
-log.addHandler(logging.FileHandler("test.log"))
-log.addHandler(logging.StreamHandler())
+formatter = logging.Formatter('%(asctime)s %(levelname)s %(name)s %(message)s')
+file_handler = logging.FileHandler("test.log")
+file_handler.setFormatter(formatter)
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(formatter)
+log.addHandler(file_handler)
+log.addHandler(console_handler)
 
 os.environ["TZ"] = "UTC"
 time.tzset()
