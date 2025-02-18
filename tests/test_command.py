c71cbb2d5ba38dcb776eda0bd318f2bc7f25f11f
Bryn M. Reeves <bmr@redhat.com>
2025-02-18 19:15:47 +0000

    dist: clean up copyright statements and convert to SPDX license headers
    Resolves: #106.

--- tests/test_command.py
+++ tests/test_command.py
@@ -1,16 +1,10 @@
-# Copyright (C) 2023 Red Hat, Inc., Bryn M. Reeves <bmr@redhat.com>
+# Copyright Red Hat
 #
-# tests/test_manager.py - Plugin core unit tests
+# tests/test_command.py - CLI layer tests
 #
 # This file is part of the snapm project.
 #
-# This copyrighted material is made available to anyone wishing to use,
-# modify, copy, or redistribute it subject to the terms and conditions
-# of the GNU General Public License v.2.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software Foundation,
-# Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+# SPDX-License-Identifier: GPL-2.0-only
 import unittest
 import logging
 import os
