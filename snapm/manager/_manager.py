trgill/snapm

commit 9b3cb78652877334c9512967633995dde32e8689

snapm: move boot entry creation inside Manager.create_snapshot_set()
Simplifies CLI and makes creation of a snapshot set and its boot entries
and indivisible operation.

Resolves: #104

snapm/manager/_manager.py
@@ -871,9 +871,11 @@ class Manager:
                     f"Snapshot set name cannot include '{char}'"
                 )
 
-    # pylint: disable=too-many-branches,too-many-locals
+    # pylint: disable=too-many-branches,too-many-locals,too-many-statements
     @suspend_signals
-    def create_snapshot_set(self, name, source_specs, default_size_policy=None):
+    def create_snapshot_set(
+        self, name, source_specs, default_size_policy=None, boot=False, revert=False
+    ):
         """
         Create a snapshot set of the supplied mount points with the name
         ``name``.
@@ -967,6 +969,26 @@ class Manager:
         snapset = SnapshotSet(name, timestamp, snapshots)
         for snapshot in snapset.snapshots:
             snapshot.snapshot_set = snapset
+            if boot or revert:
+                snapshot.autoactivate = True
+                snapshot.activate()
+
+        if boot:
+            try:
+                create_snapset_boot_entry(snapset)
+            except (OSError, ValueError) as err:
+                _log_error("Failed to create snapshot set boot entry: %s", err)
+                snapset.delete()
+                raise SnapmCalloutError from err
+
+        if revert:
+            try:
+                create_snapset_revert_entry(snapset)
+            except (OSError, ValueError) as err:
+                _log_error("Failed to create snapshot set revert boot entry: %s", err)
+                snapset.delete()
+                raise SnapmCalloutError from err
+
         self.by_name[snapset.name] = snapset
         self.by_uuid[snapset.uuid] = snapset
         self.snapshot_sets.append(snapset)

snapm/command.py
@@ -476,30 +476,9 @@ def create_snapset(manager, name, sources, size_policy=None, boot=False, revert=
     :param boot: Create a boot entry for this snapshot set.
     :param revert: Create a revert boot entry for this snapshot set.
     """
-    snapset = manager.create_snapshot_set(
-        name, sources, default_size_policy=size_policy
+    return manager.create_snapshot_set(
+        name, sources, default_size_policy=size_policy, boot=boot, revert=revert
     )
-
-    # Snapshot sets must be active to create boot entries.
-    if boot or revert:
-        select = Selection(name=snapset.name)
-        manager.activate_snapshot_sets(select)
-
-    if boot:
-        try:
-            manager.create_snapshot_set_boot_entry(name=snapset.name)
-        except (OSError, ValueError, TypeError) as err:
-            _log_error("Failed to create snapshot set boot entry: %s", err)
-            manager.delete_snapshot_sets(select)
-            return None
-    if revert:
-        try:
-            manager.create_snapshot_set_revert_entry(name=snapset.name)
-        except (OSError, ValueError, TypeError) as err:
-            _log_error("Failed to create snapshot set revert boot entry: %s", err)
-            manager.delete_snapshot_sets(select)
-            return None
-    return snapset
 
 
 def delete_snapset(manager, selection):

commit 8cd4d108457c7367957b4a46e73f4969135055ba

Manager: fix autoactivate setting during snapshot set creation
Fix a regression introduced in commit 9b3cb78: attempting to set
snapshot.autoactivate fails:

  # snapm snapset create -br upgrade / /var
  ERROR - Command failed: property 'autoactivate' of 'Lvm2CowSnapshot' object has no setter

This affects any snapshot set created with --bootable/--revert.

Instead of looping over a SnapshotSet's members and setting the
autoactivate property, just set SnapshotSet.autoactivate, and let the
SnapshotSet class take care of propagating the setting to individual
snapshots.

This fixes a bug which prevents creation of bootable snapshot sets, or
revert boot entries due to Manager.create_snapshot_set() attempting to
set the read-only Snapshot.autoactivate property.

Resolves: #145

snapm/manager/_manager.py
@@ -965,10 +965,10 @@ class Manager:
             provider.end_transaction()
 
         snapset = SnapshotSet(name, timestamp, snapshots)
-        for snapshot in snapset.snapshots:
-            if boot or revert:
-                snapshot.autoactivate = True
-                snapshot.activate()
+
+        if boot or revert:
+            snapset.autoactivate = True
+            snapset.activate()
 
         if boot:
             try: