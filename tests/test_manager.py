trgill/snapm

commit a32e85f65aaec12b4a18ef910bf7a00385299464

snapm: add Manager.split_snapshot_set()
Add a method to allow splitting (dividing one snapshot set into two new
snapshot sets according to a list of sources to be split), and pruning
(deleting a list of sources from an existing snapshot set).

When called with 'name', 'new_name', and 'source_specs' the method will
divide the snapshot set named 'name' into 'name' and 'new_name', with
all sources listed in 'source_specs' moved to snapshot set 'new_name'.

When called with 'name', new_name=None, and 'source_specs' the method
will prune (delete) the given 'source_specs' from the snapshot set named
'name'.

It is considered an error and will raise SnapmArgumentError if the given
list of sources is either empty, or would leave the original snapshot set
empty following the split/prune operation.

Resolves: #174

snapm/manager/_manager.py
@@ -44,6 +44,7 @@ from snapm import (
     SnapmPluginError,
     SnapmStateError,
     SnapmRecursionError,
+    SnapmArgumentError,
     Selection,
     bool_to_yes_no,
     is_size_policy,
@@ -1235,6 +1236,119 @@ class Manager:
             self._set_autoactivate(snapset, auto=auto)
             changed += 1
         return changed
+
+    @suspend_signals
+    def split_snapshot_set(self, name, new_name, source_specs):
+        """
+        Split the snapshot set named `name` into two snapshot sets, with
+        `new_name` containing the listed `sources` and `name` containing all
+        remaining snapshots.
+
+        If `new_name` is `None` the listed snapshot are removed from `name`
+        and permanently deleted. This operation cannot be undone.
+
+        :param name: The name of the snapshot set to split
+        :param new_name: The name of the newly split off snapshot set
+        :param sources: The list of sources to include in the new snapshot set
+        """
+        if name not in self.by_name:
+            raise SnapmNotFoundError(f"Cannot find snapshot set named {name}")
+
+        if new_name:
+            self._validate_snapset_name(new_name)
+
+        op = f"{'split' if new_name else 'prune'}"
+
+        _log_info("Attempting to %s snapshot set '%s'", op, name)
+
+        snapset = self.by_name[name]
+        _check_snapset_status(snapset, op)
+
+        # Parse size policies and normalise mount paths
+        (sources, size_policies) = _parse_source_specs(source_specs, None)
+
+        if any(size_policies[source] is not None for source in sources):
+            err_sources = ", ".join(
+                [source for source in source_specs if ":" in source]
+            )
+            raise SnapmArgumentError(
+                f"SnapshotSet {op} does not support size policies: {err_sources}"
+            )
+
+        missing_sources = [
+            source for source in sources if source not in snapset.sources
+        ]
+
+        if missing_sources:
+            raise SnapmNotFoundError(
+                f"SnapshotSet '{snapset.name}' does not contain {', '.join(missing_sources)}"
+            )
+
+        timestamp = snapset.timestamp
+
+        split_snapshots = [
+            snapshot for snapshot in snapset.snapshots if snapshot.source in sources
+        ]
+
+        keep_snapshots = [
+            snapshot for snapshot in snapset.snapshots if snapshot.source not in sources
+        ]
+
+        if not keep_snapshots:
+            op_str = "Splitting" if new_name else "Pruning"
+            raise SnapmArgumentError(
+                f"{op_str} {', '.join((snap.source for snap in split_snapshots))} "
+                f"from {snapset.name} would leave {snapset.name} empty"
+            )
+
+        _log_debug(
+            "Splitting snapshot%s %s from snapshot set '%s'",
+            "s" if len(split_snapshots) > 1 else "",
+            ", ".join((snap.source for snap in split_snapshots)),
+            snapset.name,
+        )
+
+        self.by_name.pop(snapset.name)
+        self.by_uuid.pop(snapset.uuid)
+        self.snapshot_sets.remove(snapset)
+
+        # Build new SnapshotSet with old name to start with, then use
+        # SnapshotSet.rename() to propagate new_name to members.
+        new_set = SnapshotSet(snapset.name, timestamp, split_snapshots)
+
+        # Update original snapshot set and re-add to lookup tables
+        orig_set = SnapshotSet(snapset.name, timestamp, keep_snapshots)
+        self.by_name[orig_set.name] = orig_set
+        self.by_uuid[orig_set.uuid] = orig_set
+        self.snapshot_sets.append(orig_set)
+
+        if new_name:
+            # Attempt to rename members to new_name
+            try:
+                new_set.rename(new_name)
+            except SnapmError as err:
+                self.by_name[snapset.name] = snapset
+                self.by_uuid[snapset.uuid] = snapset
+                self.snapshot_sets.append(snapset)
+                raise err
+        else:
+            try:
+                # Delete the pruned members
+                new_set.delete()
+            except SnapmError as err:
+                _log_error(
+                    "Failed to clean up pruned snapshot set members: %s (%s)",
+                    err,
+                    ", ".join(snapshot.name for snapshot in new_set.snapshots),
+                )
+            return orig_set
+
+        # Add new snapshot set to lookup tables
+        self.by_name[new_set.name] = new_set
+        self.by_uuid[new_set.uuid] = new_set
+        self.snapshot_sets.append(new_set)
+
+        return new_set
 
     @suspend_signals
     def create_snapshot_set_boot_entry(self, name=None, uuid=None):

commit 8032906b101bd1dd2302dfd855c9b41aad763970

snapm: fix Manager.split_snapshot_sets() with empty sources
The snapm.manager.Manger.split_snapshot_sets() method checks whether the
original snapshot set would be empty after the operation and raises an
error if this is the case. There is no corresponding check to ensure
that the destination snapshot set is not empty (i.e. the sources passed
to the split_snapshot_set() method were empty).

The snapm.command wrappers already require the sources argument:

  root@localhost:~/src/git/snapm# snapm snapset split upgrade noupgrade
  usage: snapm snapset split [-h] NAME NEW_NAME SOURCE [SOURCE ...]
  snapm snapset split: error: the following arguments are required: SOURCE

But the API does not enforce this, leading to a snapshot set with no
sources and NrSnapshots == 0:

  >>> p = m.split_snapshot_set("upgrade", "noupgrade", [])
  DEBUG - Blocking termination signals {<Signals.SIGINT: 2>, <Signals.SIGTERM: 15>}
  INFO - Attempting to split snapshot set 'upgrade'
  DEBUG - Splitting snapshot  from snapshot set 'upgrade'
  INFO - Attempting rename for snapshot set 'upgrade' to 'noupgrade'
  DEBUG - Unblocking termination signals {<Signals.SIGINT: 2>, <Signals.SIGTERM: 15>}
  >>> p
  <snapm._snapm.SnapshotSet object at 0x7eff5a90d220>
  >>> print(p)
  SnapsetName:      noupgrade
  Sources:
  NrSnapshots:      0
  Time:             2025-04-01 15:02:37
  UUID:             fab385b4-5912-54f6-8458-9b487eaf7b50
  Status:           Active
  Autoactivate:     yes
  Bootable:         no

In practice this makes the call a no-op but it's pointless and confusing
to allow this combination of arguments: raise a SnapmArgumentError if
the source_specs list is empty or None.

Fixes: a32e85f
Related: #98, #174
Resolves: #179

snapm/manager/_manager.py
@@ -1264,6 +1264,11 @@ class Manager:
         snapset = self.by_name[name]
         _check_snapset_status(snapset, op)
 
+        if not source_specs:
+            raise SnapmArgumentError(
+                f"SnapshotSet {op} requires at least one source argument"
+            )
+
         # Parse size policies and normalise mount paths
         (sources, size_policies) = _parse_source_specs(source_specs, None)
 

tests/test_manager.py
@@ -844,6 +844,15 @@ class ManagerTests(unittest.TestCase):
             # Split non-existent source from snapshot set
             split = self.manager.split_snapshot_set(testset, splitset, [split_source])
 
+    def test_split_snapshot_set_split_empty_sources(self):
+        testset = "testset0"
+        splitset = "testset1"
+        self.manager.create_snapshot_set(testset, self.mount_points())
+
+        with self.assertRaises(snapm.SnapmArgumentError):
+            # Split [] from snapshot set
+            split = self.manager.split_snapshot_set(testset, splitset, [])
+
     def test_split_snapshot_set_prune(self):
         testset = "testset0"
         self.manager.create_snapshot_set(testset, self.mount_points())
@@ -878,6 +887,14 @@ class ManagerTests(unittest.TestCase):
         with self.assertRaises(snapm.SnapmArgumentError):
             # Split split_source from snapshot set
             split = self.manager.split_snapshot_set(testset, None, split_sources)
+
+    def test_split_snapshot_set_prune_empty_sources(self):
+        testset = "testset0"
+        self.manager.create_snapshot_set(testset, self.mount_points())
+
+        with self.assertRaises(snapm.SnapmArgumentError):
+            # Split [] from snapshot set
+            split = self.manager.split_snapshot_set(testset, None, [])
 
     def test_split_snapshot_set_prune_size_policy_raises(self):
         testset = "testset0"