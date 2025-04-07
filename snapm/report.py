06ad9d3b5382227f527586b8d751127f7e73845a
Bryn M. Reeves <bmr@redhat.com>
2025-04-07 02:36:22 +0100

    report: Field - stop misusing class vars
    Resolves: #201

--- snapm/report.py
+++ snapm/report.py
@@ -328,14 +328,6 @@ class Field:
     """
 
     #: reference to the containing Report
-    report = None
-    #: reference to the FieldProperties describing this field
-    props = None
-    #: The formatted string to be reported for this field.
-    report_string = None
-    #: The raw value of this field. Used for sorting.
-    sort_value = None
-
     def __init__(self, report, props):
         """
         Initialise a new Field object.
@@ -348,6 +340,8 @@ class Field:
         """
         self.report = report
         self.props = props
+        self.report_string = ""
+        self.sort_value = None
 
     def report_str(self, value):
         """
