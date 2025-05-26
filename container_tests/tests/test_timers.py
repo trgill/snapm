e4ffda8a1d70578b694a3c3bd2c8ce5f00ec0e7a
Bryn M. Reeves <bmr@redhat.com>
2025-05-26 20:22:53 +0100

    container_tests: update test_timers for private low-level interface
    Related: #272

--- container_tests/tests/test_timers.py
+++ container_tests/tests/test_timers.py
@@ -4,13 +4,14 @@ import os.path
 
 import snapm
 from snapm.manager._timers import (
-    timer,
-    TIMER_ENABLE,
-    TIMER_DISABLE,
-    TIMER_START,
-    TIMER_STOP,
-    UNIT_CREATE,
-    UNIT_GC,
+    # low-level interface
+    _timer,
+    _TIMER_ENABLE,
+    _TIMER_DISABLE,
+    _TIMER_START,
+    _TIMER_STOP,
+    _UNIT_CREATE,
+    _UNIT_GC,
 )
 from snapm.manager._calendar import CalendarSpec
 
@@ -30,97 +31,97 @@ class TimerTests(unittest.TestCase):
         self.assertEqual(enabled, os.path.exists(drop_in_file))
 
     def enable_assert_disable_timer(self, unit: str, instance: str, calendarspec: str):
-        timer(TIMER_ENABLE, unit, instance, calendarspec=CalendarSpec(calendarspec))
+        _timer(_TIMER_ENABLE, unit, instance, calendarspec=CalendarSpec(calendarspec))
         self.assert_paths(unit, instance, True)
 
-        timer(TIMER_START, unit, instance)
+        _timer(_TIMER_START, unit, instance)
         # assert unit status
 
-        timer(TIMER_STOP, unit, instance)
+        _timer(_TIMER_STOP, unit, instance)
         # assert unit status
 
-        timer(TIMER_DISABLE, unit, instance)
+        _timer(_TIMER_DISABLE, unit, instance)
         self.assert_paths(unit, instance, False)
 
     def test_create_timer_enable_daily(self):
-        self.enable_assert_disable_timer(UNIT_CREATE, "daily", "daily")
+        self.enable_assert_disable_timer(_UNIT_CREATE, "daily", "daily")
 
     def test_gc_timer_enable_daily(self):
-        self.enable_assert_disable_timer(UNIT_GC, "daily", "daily")
+        self.enable_assert_disable_timer(_UNIT_GC, "daily", "daily")
 
     def test_create_timer_enable_hourly(self):
-        self.enable_assert_disable_timer(UNIT_CREATE, "hourly", "hourly")
+        self.enable_assert_disable_timer(_UNIT_CREATE, "hourly", "hourly")
 
     def test_gc_timer_enable_hourly(self):
-        self.enable_assert_disable_timer(UNIT_GC, "hourly", "hourly")
+        self.enable_assert_disable_timer(_UNIT_GC, "hourly", "hourly")
 
     def test_create_timer_enable_weekly(self):
-        self.enable_assert_disable_timer(UNIT_CREATE, "weekly", "weekly")
+        self.enable_assert_disable_timer(_UNIT_CREATE, "weekly", "weekly")
 
     def test_gc_timer_enable_weekly(self):
-        self.enable_assert_disable_timer(UNIT_GC, "weekly", "weekly")
+        self.enable_assert_disable_timer(_UNIT_GC, "weekly", "weekly")
 
     def test_create_timer_enable_monthly(self):
-        self.enable_assert_disable_timer(UNIT_CREATE, "monthly", "monthly")
+        self.enable_assert_disable_timer(_UNIT_CREATE, "monthly", "monthly")
 
     def test_gc_timer_enable_monthly(self):
-        self.enable_assert_disable_timer(UNIT_GC, "monthly", "monthly")
+        self.enable_assert_disable_timer(_UNIT_GC, "monthly", "monthly")
 
     def test_create_timer_enable_thu_fri_1_5_11(self):
         self.enable_assert_disable_timer(
-            UNIT_CREATE, "thu_fri_1_5_11", "Thu,Fri *-*-1,5 11:12:13"
+            _UNIT_CREATE, "thu_fri_1_5_11", "Thu,Fri *-*-1,5 11:12:13"
         )
 
     def test_gc_timer_enable_thu_fri_1_5_11(self):
         self.enable_assert_disable_timer(
-            UNIT_GC, "thu_fri_1_5_11", "Thu,Fri *-*-1,5 11:12:13"
+            _UNIT_GC, "thu_fri_1_5_11", "Thu,Fri *-*-1,5 11:12:13"
         )
 
     def test_create_timer_enable_sat_sun_8(self):
-        self.enable_assert_disable_timer(UNIT_CREATE, "sat_sun_8", "Sat,Sun 08:05:40")
+        self.enable_assert_disable_timer(_UNIT_CREATE, "sat_sun_8", "Sat,Sun 08:05:40")
 
     def test_gc_timer_enable_sat_sun_8(self):
-        self.enable_assert_disable_timer(UNIT_GC, "sat_sun_8", "Sat,Sun 08:05:40")
+        self.enable_assert_disable_timer(_UNIT_GC, "sat_sun_8", "Sat,Sun 08:05:40")
 
     def test_timer_enable_empty_calendarspec_raises(self):
         with self.assertRaises(snapm.SnapmArgumentError) as cm:
-            timer(TIMER_ENABLE, UNIT_CREATE, "hourly", calendarspec="")
+            _timer(_TIMER_ENABLE, _UNIT_CREATE, "hourly", calendarspec="")
             self.assertTrue("requires non-empty calendarspec" in cm.exception)
 
     def test_timer_enable_bad_calendarspec_raises(self):
         with self.assertRaises(snapm.SnapmArgumentError) as cm:
-            timer(TIMER_ENABLE, UNIT_CREATE, "hourly", calendarspec="quux:foo")
+            _timer(_TIMER_ENABLE, _UNIT_CREATE, "hourly", calendarspec="quux:foo")
             self.assertTrue("invalid calendarspec string" in cm.exception)
 
     def test_timer_bad_op_raises(self):
         with self.assertRaises(snapm.SnapmArgumentError) as cm:
-            timer("QUUX", UNIT_CREATE, "hourly", calendarspec="hourly")
+            _timer("QUUX", _UNIT_CREATE, "hourly", calendarspec="hourly")
             self.assertTrue("Invalid timer operation: QUUX" in cm.exception)
 
     def test_timer_bad_unit_raises(self):
         with self.assertRaises(snapm.SnapmArgumentError) as cm:
-            timer(TIMER_ENABLE, "quux", "hourly", calendarspec="hourly")
+            _timer(_TIMER_ENABLE, "quux", "hourly", calendarspec="hourly")
             self.assertTrue("Invalid timer unit" in cm.exception)
 
     def test_timer_empty_instance_raises(self):
         with self.assertRaises(snapm.SnapmArgumentError) as cm:
-            timer(TIMER_ENABLE, UNIT_CREATE, "", calendarspec="hourly")
+            _timer(_TIMER_ENABLE, _UNIT_CREATE, "", calendarspec="hourly")
             self.assertTrue("Timer instance cannot be empty" in cm.exception)
 
     def test_timer_none_instance_raises(self):
         with self.assertRaises(snapm.SnapmArgumentError) as cm:
-            timer(TIMER_ENABLE, UNIT_CREATE, None, calendarspec="hourly")
+            _timer(_TIMER_ENABLE, _UNIT_CREATE, None, calendarspec="hourly")
             self.assertTrue("Timer instance cannot be empty" in cm.exception)
 
     def test_timer_bad_op_with_calendarspec_raises(self):
         with self.assertRaises(snapm.SnapmArgumentError) as cm:
-            timer(TIMER_START, UNIT_CREATE, "hourly", calendarspec="hourly")
+            _timer(_TIMER_START, _UNIT_CREATE, "hourly", calendarspec="hourly")
             self.assertTrue("does not accept calendarspec" in cm.exception)
 
         with self.assertRaises(snapm.SnapmArgumentError) as cm:
-            timer(TIMER_STOP, UNIT_CREATE, "hourly", calendarspec="hourly")
+            _timer(_TIMER_STOP, _UNIT_CREATE, "hourly", calendarspec="hourly")
             self.assertTrue("does not accept calendarspec" in cm.exception)
 
         with self.assertRaises(snapm.SnapmArgumentError) as cm:
-            timer(TIMER_DISABLE, UNIT_CREATE, "hourly", calendarspec="hourly")
+            _timer(_TIMER_DISABLE, _UNIT_CREATE, "hourly", calendarspec="hourly")
             self.assertTrue("does not accept calendarspec" in cm.exception)
