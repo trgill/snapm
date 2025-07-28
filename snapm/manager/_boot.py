513b79e73762375ab78936183f13225da7e2b2c7
Bryn M. Reeves <bmr@redhat.com>
2025-07-28 20:39:38 +0100

    boot: always return path from _find_snapset_root()
    Change _find_snapset_root() to always return a device path, rather than
    a Snapshot object. Looking up a device from the fstab will always
    produce a device path so this is required in order to extend
    _find_snapset_root() to support snapshot sets that do not include the
    root file system.
    
    Related: #344

--- snapm/manager/_boot.py
+++ snapm/manager/_boot.py
@@ -97,7 +97,7 @@ def _find_snapset_root(snapset):
     """
     for snapshot in snapset.snapshots:
         if snapshot.mount_point == "/":
-            return snapshot
+            return snapshot.devpath
     # Note: add fstab lookup for non-root snapsets
     # needs either root=UUID/LABEL support in boom or a lookup to resolve any
     # UUID/LABEL found in the file.
