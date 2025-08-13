49c48954074660225f6a902726d0dc8f579767db
Bryn M. Reeves <bmr@redhat.com>
2025-08-13 21:23:20 +0100

    tests: add cleanup script to tests/bin
    Add a simple cleanup script to remove test artefacts after a failure to
    automatically tear down.
    
    Related: #380

---
+++ tests/bin/cleanup.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+if [[ "$1" != "--force" ]]; then
+    echo -n "Clean up test suite mounts and devices? (y/n): "
+    read YES
+    if [[ "$YES" != "y" ]]; then
+        exit 0
+    fi
+fi
+
+umount /var/tmp/*_snapm_mounts/* &> /dev/null
+
+if [ -f /tmp/fstab ]; then
+    umount /etc/fstab &> /dev/null
+    rm -f /tmp/fstab
+fi
+
+if [ -d /dev/test_vg0 ]; then
+	export LVM_SYSTEM_DIR="$PWD/tests/lvm"
+	vgremove --force --yes test_vg0 &> /dev/null || echo Failed to clean up test_vg0
+fi
+
+if [ -d /dev/stratis/pool1 ]; then
+	STRATIS_FILESYSTEMS=$(stratis filesystem list pool1 | awk '/pool1/{print $2}')
+	for FS in $STRATIS_FILESYSTEMS; do
+	    stratis filesystem destroy pool1 $FS &> /dev/null || echo Failed to clean up pool1 $FS
+	done
+	stratis pool destroy pool1 &> /dev/null || echo Failed to clean up pool1
+fi
+
+LOOP_DEVICES=$(losetup --noheadings --list -Oname,back-file | awk '/_snapm_loop_back/{print $1}')
+for loop in $LOOP_DEVICES; do
+    losetup -d $loop || echo Failed to clean up loop device $loop
+done
+
+rm -rf /var/tmp/*_snapm_loop_back
+rm -rf /var/tmp/*_snapm_mounts
+rm -rf /var/tmp/*_snapm_boom_dir
